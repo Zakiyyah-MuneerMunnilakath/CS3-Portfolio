864bb32f0557da3a95ca5386d7554379bce6589b
Zakiyyah-MuneerMunnilakath <mpmuneermunnilakath2030@clc.pshs.edu.ph>
2026-08-16 20:29:21 +0800

    Update q1_sg2_a3.py

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,8 +1,12 @@
-birth = input(int("Enter year of birth: "))
+zodiac = ["Rat (鼠 / Shǔ)", "Ox (牛 / Niú)","Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)",
+        "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ", "Goat (羊 / Yáng)",
+        "Monkey (猴 / Hóu)","Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]
 
-if birth >= 1900:
-  
+birth = int(input("Enter year of birth: "))
 
-
-else:
-  print("Invalid year, it should not be earlier than 1900.")
+if birth < 1900: 
+    print("Invalid Year, it should not be earlier than 1900.")
+    exit()
+else: 
+    sign = (birth-1900)%12
+    print("Your Chinese Zodiac Sign is:", zodiac[sign])
